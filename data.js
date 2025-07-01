8e5e2a2281807c70b389df9720cfd6b0be24eff7
rexz26hff <bg185967@gmail.com>
2025-07-02 01:30:56 +0700

    Add token via bot

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8103529028:AAGWdf_P6So2z9OmBWd0Go9nmLBj6UxBYms",
-    "7747013602:AAGshW7UTwhaVA64X0SEXhYfVoQlkepLPQw"
+    "7747013602:AAGshW7UTwhaVA64X0SEXhYfVoQlkepLPQw",
+    "7893449898:AAEwKQckhoXoKMXbL5GNQnc4voaoTPbicKI"
   ]
 }
